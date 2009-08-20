0caca1c4e128465126332bc03e4983137cfcfb7e
pH <pedrohfranceschi@gmail.com>
2009-08-20 20:14:25 -0300

    Initial Import

---
+++ libgenesis.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <avr/pgmspace.h>
+#include <inttypes.h>
+#include <avr/pgmspace.h>
+#include "../libnerdkits/delay.h"
+#include "../libnerdkits/lcd.h"
+#include "../libnerdkits/uart.h"
+
