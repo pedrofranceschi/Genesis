1d40a60be162c927b4668665936944c9e5de908d
pH <pedrohfranceschi@gmail.com>
2009-08-22 12:11:51 -0300

    Added some comments to lcd_message sample code.

--- sample-codes/lcd_message/lcd_message.c
+++ sample-codes/lcd_message/lcd_message.c
@@ -1,14 +1,14 @@
-#include "../../libgenesis.h"
+#include "../../libgenesis.h" // include the main lib
 int main()
 {
 	lcd_start(); // start the LCD
 	while (1) // loop
 	{
-	sleep(1);
+	sleep(1); // wait a second
 	lcd_print(PSTR("LibGenesis - Simple "), 1); // print the message in line 1
 	lcd_print(PSTR("MCU programming lib."), 2); // print the message in line 2
 	lcd_print(PSTR("by pH - iBlogeek.com"), 4); // print the message in line 4
-	sleep(1);
-	clear_lcd();
+	sleep(1); // wait a second
+	clear_lcd(); // remove everything from LCD 
 }
 }
